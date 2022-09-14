61fc3f641b78cf2feb7a0c43c26ea49beb74b1e7
Noncorporeal <42417289+Noncorporeal@users.noreply.github.com>
2022-09-14 15:35:38 -0600

    Add project files.

---
+++ First_NET_project/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_NET_project
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = "Christoph";
+			string location = "Tucson, AZ";
+
+			Console.WriteLine($"My name is {name}, I am from {location}");
+			DateTime date = DateTime.Now;
+			Console.WriteLine($"Today's date is {date.ToString("d")}");
+			DateTime christmas = new DateTime(2022, 12, 25);
+			Console.WriteLine("There are {0} days till Christmas!", (christmas - date).TotalDays);
+
+
+			double width, height, woodLength, glassArea;
+			string widthString, heightString;
+			Console.Write("Width Input:");
+			widthString = Console.ReadLine();
+			
+			width = double.Parse(widthString);
+			Console.Write("Height Input: ");
+			heightString = Console.ReadLine();
+
+			height = double.Parse(heightString);
+			woodLength = 2 * (width + height) * 3.25;
+			glassArea = 2 * (width * height);
+
+			Console.WriteLine("The length of the wood is " +
+			woodLength + " feet");
+
+			Console.WriteLine("The area of the glass is " +
+			glassArea + " square metres");
+
+			Console.WriteLine("Press Enter to Exit...");
+			while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+
+		}
+	}
+}
